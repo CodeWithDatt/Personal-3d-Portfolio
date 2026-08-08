a65b8413a2898d7470bddd9f91624da8b245ec10
Datt Patel <dattpatel207@gmail.com>
2026-08-08 09:41:18 +0530

    Initial portfolio development

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section className='border border-red-500 h-dvh relative text-white-50 px-5 md:p-0'>
+        Hero
+    </section>
+  )
+}
+
+export default Hero
